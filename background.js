c8f2b15d29777c95678b78ee5d5a757df4f00da0
Mohammad Khani <soheyl637@gmail.com>
2025-02-18 19:57:43 +0000

    Add comments to background.js for clarity

--- background.js
+++ background.js
@@ -1,3 +1,6 @@
+// background.js
+
+// Listen for extension icon clicks and send a message to enable selection mode
 chrome.action.onClicked.addListener((tab) => {
-    chrome.tabs.sendMessage(tab.id, { action: "enable_selection" });
+  chrome.tabs.sendMessage(tab.id, { action: "enable_selection" });
 });
