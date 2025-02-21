5645bfdfc2160d663c34f2ce0a8a35a4506be8a5
Mohammad Khani <soheyl637@gmail.com>
2025-02-21 12:34:07 +0000

    fix: update Persian regex to allow alphanumeric characters and special symbols

--- content.js
+++ content.js
@@ -12,7 +12,7 @@
 
 // Regex patterns to detect Persian characters and RTL content
 const PERSIAN_REGEX =
-  /^[\u0600-\u06FF\u0660-\u0669\u06F0-\u06F9\s.,:;؟!()«»\n\t\u200C]+$/;
+  /^(?=.*[\u0600-\u06FF])[\u0600-\u06FF\u0660-\u0669\u06F0-\u06F9\u0041-\u005A\u0061-\u007A\u0030-\u0039\s.,:;؟!()«»@#\n\t\u200C]+$/;
 const RTL_REGEX = /[\u0600-\u06FF]/;
 
 // State management for selection and translation icon
