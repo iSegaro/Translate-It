d3f6eca7c49ed3178efff68414078662e677c0e2
Mohammad Khani <soheyl637@gmail.com>
2025-08-27 15:36:42 +0100

    feat: Replace SVG icon with image for Translate button in TextFieldIcon component

--- src/views/content/components/TextFieldIcon.vue
+++ src/views/content/components/TextFieldIcon.vue
@@ -7,7 +7,13 @@
     @mouseup.prevent.stop
     title="Translate with Translate-It"
   >
-    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="currentColor" d="M12.87 15.07l-2.54-2.51.03-.03c1.74-1.94 2.98-4.17 3.71-6.53H17V4h-7V2H8v2H1v1.99h11.17C11.5 7.92 10.44 9.75 9 11.35c-.98 1.08-2.27 2.3-3.66 3.58l-1.41-1.42L1 16l3.89 3.89l1.42-1.42L4.16 16.34c1.4-1.25 2.7-2.48 3.66-3.58c1.13-1.27 2.11-2.62 2.91-4.01H4.22V6.97h12.62c-.29 1.45-.83 2.84-1.59 4.1l-2.15-2.15l-.03.03l-2.54 2.51zM21.33 16.12l-7.19-7.19l-1.41 1.41l7.19 7.19z"/></svg>
+    <img
+      src="@/assets/icons/extension_icon_64.svg"
+      alt="Translate Icon"
+      width="16"
+      height="16"
+      style="display: block; pointer-events: none;"
+    />
   </button>
 </template>
 
