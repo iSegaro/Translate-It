aefe093fef3fad13e839e70390f44d2e5fe8e2c2
Mohammad Khani <soheyl637@gmail.com>
2025-08-31 14:05:12 +0100

    fix: Refactor LanguageSelector component for improved structure and RTL support

--- src/components/feature/LanguageSelector.vue
+++ src/components/feature/LanguageSelector.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="language-selector">
-    <select
-      :value="modelValue"
-      :disabled="disabled"
-      class="language-select"
-      @change="handleChange"
+  <select
+    :value="modelValue"
+    :disabled="disabled"
+    class="language-select"
+    @change="handleChange"
+  >
+    <option 
+      v-for="language in languages" 
+      :key="language.code" 
+      :value="language.code"
     >
-      <option 
-        v-for="language in languages" 
-        :key="language.code" 
-        :value="language.code"
-      >
-        {{ language.name }}
-      </option>
-    </select>
-  </div>
+      {{ language.name }}
+    </option>
+  </select>
 </template>
 
 <script setup>
@@ -46,10 +44,6 @@ const handleChange = (event) => {
 </script>
 
 <style scoped>
-.language-selector {
-  flex: 1;
-}
-
 .language-select {
   width: 100%;
   padding: 6px 8px;
@@ -59,6 +53,15 @@ const handleChange = (event) => {
   background-color: var(--color-background);
   color: var(--color-text);
   cursor: pointer;
+  
+  /* Custom dropdown arrow for better RTL support */
+  appearance: none;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='12' height='12' viewBox='0 0 12 12'%3E%3Cpath fill='%23666' d='M6 8L2 4h8z'/%3E%3C/svg%3E");
+  background-repeat: no-repeat;
+  background-position: right 8px center;
+  padding-right: 30px;
   
   &:focus {
     outline: none;
@@ -70,5 +73,13 @@ const handleChange = (event) => {
     cursor: not-allowed;
     background-color: var(--color-surface);
   }
+}
+
+/* RTL support for dropdown arrow */
+:global(.extension-options.rtl) .language-select {
+  background-position: left 8px center;
+  padding-right: 8px;
+  padding-left: 30px;
+  direction: rtl;
 }
 </style>
