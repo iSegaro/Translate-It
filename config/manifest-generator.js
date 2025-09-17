18db602edf6da937038efb7527fba926d620be6f
Mohammad Khani <soheyl637@gmail.com>
2025-09-17 22:34:33 +0100

    config(manifest): remove data collection permissions
    Removed the data_collection_permissions section from the Firefox manifest configuration while keeping the strict_min_version requirement for Firefox 112+.

--- config/manifest-generator.js
+++ config/manifest-generator.js
@@ -168,10 +168,7 @@ function generateFirefoxManifest(baseManifest) {
     browser_specific_settings: {
       gecko: {
         id: 'ai-writing-companion@amm1rr.com',
-        strict_min_version: '112.0', // Firefox 112+ for background.type module support
-        data_collection_permissions: {
-          required: ["none"]
-        }
+        strict_min_version: '112.0' // Firefox 112+ for background.type module support
       }
     },
 
