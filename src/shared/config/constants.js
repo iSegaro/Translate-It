53069a4fb16144ac4f728d7429ba987f59e223c4
Mohammad Khani <soheyl637@gmail.com>
2025-10-09 05:32:40 +0100

    config: change default target language to Persian
    Updates the default target language constant from English ("en") to Persian ("fa") in shared configuration. This change modifies the default language setting used throughout the extension when no specific language is selected.

--- src/shared/config/constants.js
+++ src/shared/config/constants.js
@@ -2,7 +2,7 @@
 // Shared constants for the extension
 
 export const AUTO_DETECT_VALUE = "auto";
-export const DEFAULT_TARGET_LANGUAGE = "en";
+export const DEFAULT_TARGET_LANGUAGE = "fa";
 
 export const NOTIFICATION_TIME = {
   REVERT: 1500
