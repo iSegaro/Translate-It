a280db0559291bfd756000fe80dc90d46887ddb6
Mohammad Khani <m.khani2810@gmail.com>
2025-12-29 18:04:13 +0000

    chore(logging): update ElementSelection version to 2
    Update the ElementSelection value in GlobalDebugState from 3 to 2.

--- src/shared/logging/GlobalDebugState.js
+++ src/shared/logging/GlobalDebugState.js
@@ -39,7 +39,7 @@ const globalState = {
     
     // Features
     Windows: 2,
-    ElementSelection: 3,
+    ElementSelection: 2,
     TextFieldInteraction: 2,
     TextActions: 2,
     TextSelection: 2,
