5e7e3db3398bb13a1af5b274a312d4e18632a785
Mohammad Khani <m.khani2810@gmail.com>
2026-03-12 18:20:46 +0000

    style: fix placeholder alignment and direction in ApiKeyInput

--- src/components/feature/api-settings/ApiKeyInput.vue
+++ src/components/feature/api-settings/ApiKeyInput.vue
@@ -196,6 +196,14 @@ const handleTestKeys = () => {
 
   .api-key-textarea {
     width: 100%;
+
+    :deep(textarea) {
+      &::placeholder {
+        direction: rtl;
+        text-align: left;
+        unicode-bidi: plaintext;
+      }
+    }
   }
 
   .api-key-tips {
