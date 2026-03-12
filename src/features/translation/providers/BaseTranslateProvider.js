c7bb1a38bcc30c604cf3544b5c62d5b6991a0f40
Mohammad Khani <m.khani2810@gmail.com>
2026-03-12 12:56:04 +0000

    chore: remove unused ProviderNames import in BaseTranslateProvider

--- src/features/translation/providers/BaseTranslateProvider.js
+++ src/features/translation/providers/BaseTranslateProvider.js
@@ -15,7 +15,6 @@ import { LanguageSwappingService } from "@/features/translation/providers/Langua
 import { streamingManager } from "@/features/translation/core/StreamingManager.js";
 import { matchErrorToType } from '@/shared/error-management/ErrorMatcher.js';
 import { ErrorTypes } from '@/shared/error-management/ErrorTypes.js';
-import { ProviderNames } from "@/features/translation/providers/ProviderConstants.js";
 
 const logger = getScopedLogger(LOG_COMPONENTS.TRANSLATION, 'BaseTranslateProvider');
 
